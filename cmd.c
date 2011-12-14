9b27333ff2a22ebc4ea6e26a44402b93a5df4d4b
JT117 <jakoneill@gmail.com>
2011-12-14 18:00:12 +0100

    Fonction parse membres

--- cmd.c
+++ cmd.c
@@ -2,19 +2,35 @@
 
 void parse_args( cmd * c )
 {
-  char str[] ="ls -l";
   char * pch;
-
-  pch = strtok (str," |");
-  
-  while (pch != NULL)
+  int i;
+   
+  c->cmd_args = (char***)malloc( c->nb_membres * sizeof( char** ) );
+   
+  for( i = 0; i < c->nb_membres; i++ )
   {
-    printf ("%s\n",pch);
+    int j = 0;
     
+    c->cmd_args[i] = (char**)malloc( 1 * sizeof( char* ) );
+    pch = strtok ( c->membres_cmd[i]," ");
     
+    while (pch != NULL)
+    {
+      c->cmd_args[i][j] = (char*)malloc( 250 * sizeof( char ) );
+      strcpy( c->cmd_args[i][j], pch );
+      
+      printf ("%s\n",pch);
+      pch = strtok (NULL, " ");
+      j++;
+      
+      if( pch != NULL )
+      {
+	c->cmd_args[i] = (char**)realloc( c->cmd_args[i], j * sizeof( char* ) );
+      }
+    }
+    c->nb_args_membres[i] = j+1;
     
-    pch = strtok (NULL, " |");
   }
-  
+
 }
 
--- cmd.h
+++ cmd.h
@@ -1,5 +1,6 @@
 #include "string.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 typedef struct 
 {
